3e974276cf5410c8387e1503f02c92f62374df5a
Sachko-Sergey <sachkosergey11@gmail.com>
2019-11-21 00:49:02 +0300

    desktop & adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+//@prepros-append jquery.fancybox.min.js
+//@prepros-append jquery.validate.min.js
+//@prepros-append wow.min.js
+
 $(document).ready(function() {
   
   $('.slider').slick({
@@ -5,10 +9,38 @@ $(document).ready(function() {
     slidesToShow: 4,
     slidesToScroll: 2,
     dots: true,
-    autoplay: true,
+    // autoplay: true,
     autoplaySpeed: 1500,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1180,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2
+        }
+      },
+
+      {
+        breakpoint: 830,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+
+      {
+        breakpoint: 580,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+
+    ]
   });
+
+
 
 
   $('.slider-p').slick({
@@ -16,8 +48,41 @@ $(document).ready(function() {
     slidesToShow: 2,
     slidesToScroll: 2,
     dots: true,
-    autoplay: true,
+    // autoplay: true,
     autoplaySpeed: 1500,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 945,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
   });
+
+});
+
+
+$(function () {
+  
+  $('.header__burger').on('click', function (event) {
+    $('.header__burger,.header__nav').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+
+  $('.close__nav').on('click', function (event) {
+    $('.header__burger,.header__nav').removeClass('active');
+  });   
+
 });
